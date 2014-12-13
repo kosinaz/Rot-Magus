231d157da587fb74037d5ad310a486dfc952d12e
Zoli <zoltan@kosina.hu>
2014-12-13 23:52:17 +0100

    Improved actor AI

--- actor.js
+++ actor.js
@@ -1,8 +1,8 @@
-/*global ROT, ROTMAGUS*/
+/*global RM, ROT*/
 RM.Actor = function (type, ai) {
   'use strict';
   RM.scheduler.add(this, true);
-  this.type = ROTMAGUS.ACTORS[type];
+  this.type = RM.ACTORS[type];
   this.ai = ai;
   this.target = null;
   this.fov = [];
@@ -10,13 +10,20 @@ RM.Actor = function (type, ai) {
 
 RM.Actor.prototype.act = function () {
   'use strict';
+  var i, path;
+  this.computeFOV();
+  for (i = 0; i < this.neighbors; i += 1) {
+    this.computePath(this.neighbors[i].x, this.neighbors[i].y);
+  }
+  path = this.getShortest(this.paths);
+  this.moveTo(path[1][0], path[1][1]);
 };
 
-RM.Actor.prototype.setXY = function(x, y) {
+RM.Actor.prototype.moveTo = function (x, y) {
   'use strict';
   RM.map[this.x][this.y].actor = null;
-  this.x = xy[0];
-  this.y = xy[1];
+  this.x = x;
+  this.y = y;
   RM.map[this.x][this.y].actor = this;
 };
 
@@ -31,5 +38,33 @@ RM.Actor.prototype.computeFOV = function () {
   this.fov = [];
   ps.compute(this.x, this.y, 10, function (x, y) {
     this.fov.push([x, y]);
+    this.neighbors.push(RM.map[x][y].actor);
   }.bind(this));
 };
+
+RM.Actor.prototype.computePath = function (x, y) {
+  'use strict';
+  var a = new ROT.Path.AStar(x, y, function (x, y) {
+    if (RM.mapHas(x, y)) {
+      return RM.map[x][y].terrain.passable;
+    }
+    return false;
+  }.bind(this));
+  this.currentPath = [];
+  a.compute(this.x, this.y, function (x, y) {
+    this.currentPath.push([x, y]);
+  });
+  this.paths.push(this.currentPath);
+};
+
+RM.Actor.prototype.getShortest = function (paths) {
+  'use strict';
+  var i, shortest;
+  shortest = paths[0];
+  for (i = 1; i < paths.length; i += 1) {
+    shortest = shortest.length < paths[i].length ? shortest : paths[i];
+  }
+  return shortest;
+};
+
+
--- game.js
+++ game.js
@@ -65,6 +65,7 @@ RM.start = function () {
 };
 
 RM.mapHas = function (x, y) {
+  'use strict';
   if (RM.map[x]) {
     return RM.map[x][y];
   }
@@ -72,6 +73,7 @@ RM.mapHas = function (x, y) {
 };
 
 RM.isTransparent = function (x, y) {
+  'use strict';
   if (RM.mapHas(x, y)) {
     return RM.map[x][y].terrain.transparent;
   }
@@ -79,125 +81,9 @@ RM.isTransparent = function (x, y) {
 };
 
 RM.getActor = function (x, y) {
+  'use strict';
   if (RM.mapHas(x, y)) {
     return RM.map[x][y].actor;
   }
   return false;
-};
-
-ROTMAGUS.Game.prototype.onNotify = function (note, subject) {
-  'use strict';
-  var position, fov, i, x0, y0, x1, y1, xn, yn;
-  switch (note) {
-  case 'active':
-    position = this.getPosition(subject);
-    fov = this.getFOV(position);
-    switch (subject.ai) {
-    case 'guard':
-      for (i = 0; i < fov.length; i += 1) {
-        if (this.map[fov[i].x + ',' + fov[i].y].actor &&
-            !this.map[fov[i].x + ',' + fov[i].y].actor.ai) {
-          this.move(position, fov[i].x + ',' + fov[i].y);
-          subject.ai = 'move';
-          subject.target = fov[i].x + ',' + fov[i].y;
-          break;
-        }
-      }
-      break;
-    case 'move':
-      this.move(position, subject.target);
-      break;
-    default:
-      this.notify('show', {
-        center: position,
-        cells: fov
-      });
-      this.engine.lock();
-    }
-    break;
-  case 'order':
-    if (this.move(subject.from, subject.to)) {
-      this.engine.unlock();
-    }
-    break;
-  }
-};
-
-ROTMAGUS.Game.prototype.getPosition = function (entity) {
-  'use strict';
-  var cell;
-  for (cell in this.map) {
-    if (this.map.hasOwnProperty(cell)) {
-      if (this.map[cell].actor === entity) {
-        return cell;
-      }
-    }
-  }
-};
-
-ROTMAGUS.Game.prototype.move = function (from, to) {
-  'use strict';
-  var x0, y0, xn, yn, astar, at;
-  if (!this.map[to]) {
-    return false;
-  }
-  if (!this.map[to].terrain.passable) {
-    return false;
-  }
-  x0 = parseInt(from.split(',')[0], 10);
-  y0 = parseInt(from.split(',')[1], 10);
-  xn = parseInt(to.split(',')[0], 10);
-  yn = parseInt(to.split(',')[1], 10);
-  astar = new ROT.Path.AStar(xn, yn, function (x, y) {
-    if (this.map[x + ',' + y]) {
-      return this.map[x + ',' + y].terrain.passable;
-    }
-    return false;
-  }.bind(this));
-  this.path = [];
-  astar.compute(x0, y0, function (x, y) {
-    this.path.push([x, y]);
-  }.bind(this));
-  if (this.path.length > 1) {
-    at = this.path[1][0] + ',' + this.path[1][1];
-  }
-  if (!this.map[at].actor) {
-    this.map[at].actor = this.map[from].actor;
-    this.map[from].actor = null;
-    return true;
-  }
-  return false;
-};
-
-ROTMAGUS.Game.prototype.getFOV = function (position) {
-  'use strict';
-  var ps, x, y, char;
-  ps = new ROT.FOV.PreciseShadowcasting(function (x, y) {
-    if (this.map[x + ',' + y]) {
-      if (this.map[x + ',' + y].terrain) {
-        return this.map[x + ',' + y].terrain.transparent;
-      }
-    }
-    return false;
-  }.bind(this));
-  x = parseInt(position.split(',')[0], 10);
-  y = parseInt(position.split(',')[1], 10);
-  this.fov = [];
-  ps.compute(x, y, 10, function (x, y) {
-    if (this.map[x + ',' + y]) {
-      if (this.map[x + ',' + y].terrain.transparent) {
-        if (this.map[x + ',' + y].actor) {
-          char = this.map[x + ',' + y].actor.type.char;
-        } else {
-          char = this.map[x + ',' + y].terrain.char;
-        }
-        this.fov.push({
-          x: x,
-          y: y,
-          char: char
-        });
-      }
-    }
-  }.bind(this));
-  return this.fov;
 };
