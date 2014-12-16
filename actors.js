95acb7a95ae0edf189878a83e0f27b7eebb29c04
Zoli <zoltan@kosina.hu>
2014-12-16 01:00:42 +0100

    Added more monsters

--- actors.js
+++ actors.js
@@ -7,9 +7,21 @@ RM.actors = {
     'tile': '8'
   },
   'skeleton': {
-    'chance': 1,
+    'chance': 6,
     'tileX': 0,
     'tileY': 168,
     'tile': 'S'
+  },
+  'hobgoblin': {
+    'chance': 3,
+    'tileX': 24,
+    'tileY': 168,
+    'tile': 'H'
+  },
+  'troll': {
+    'chance': 1,
+    'tileX': 48,
+    'tileY': 168,
+    'tile': 'T'
   }
 };
