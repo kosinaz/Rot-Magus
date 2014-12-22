fbed9eedd2016ab0a7633cf2178e2ce05524cca6
Zoli <zoltan@kosina.hu>
2014-12-22 22:51:29 +0100

    Added more monsters and their stats

--- actors.js
+++ actors.js
@@ -2,18 +2,62 @@
 RM.actors = {
   'elf': {
     'chance': 0,
-    'tile': 'e'
+    'tile': 'e',
+    'health': 90,
+    'mana': 0,
+    'strength': 14,
+    'wisdom': 0,
+    'agility': 6,
+    'precision': 15
+  },
+  'zombie': {
+    'chance': 6,
+    'tile': 'z',
+    'health': 10,
+    'mana': 0,
+    'strength': 12,
+    'wisdom': 0,
+    'agility': 4,
+    'precision': 10
   },
   'skeleton': {
-    'chance': 6,
-    'tile': 's'
+    'chance': 5,
+    'tile': 's',
+    'health': 30,
+    'mana': 0,
+    'strength': 7,
+    'wisdom': 0,
+    'agility': 4,
+    'precision': 7
+  },
+  'goblin': {
+    'chance': 4,
+    'tile': 'g',
+    'health': 30,
+    'mana': 0,
+    'strength': 15,
+    'wisdom': 0,
+    'agility': 4,
+    'precision': 12
   },
   'hobgoblin': {
     'chance': 3,
-    'tile': 'h'
+    'tile': 'h',
+    'health': 10,
+    'mana': 0,
+    'strength': 10,
+    'wisdom': 0,
+    'agility': 4,
+    'precision': 15
   },
   'troll': {
-    'chance': 1,
-    'tile': 't'
+    'chance': 2,
+    'tile': 't',
+    'health': 100,
+    'mana': 0,
+    'strength': 20,
+    'wisdom': 0,
+    'agility': 4,
+    'precision': 7
   }
 };
