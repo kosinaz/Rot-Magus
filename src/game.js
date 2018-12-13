b3444f6905e27720ba010c7e7b020842dcb11504
kosinaz <kosinaz@gmail.com>
2018-12-13 08:19:40 +0100

    Add ROT.js FOV

--- src/game.js
+++ src/game.js
@@ -12,6 +12,7 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+const fov = new ROT.FOV.PreciseShadowcasting(isTransparent);
 let map;
 let player;
 let marker;
@@ -40,7 +41,8 @@ function create() {
   /**
    * Parameters: layer name (or index) from Tiled, tileset, x, y
    */
-  layer = map.createStaticLayer("tiles", tileset, 0, 0);
+  layer = map.createDynamicLayer("tiles", tileset, 0, 0);
+  layer.forEachTile(tile => (tile.alpha = 0));
 
   /**
    * Create player
@@ -48,6 +50,7 @@ function create() {
   const start = map.findObject("objects", obj => obj.name === "player");
   player = this.add.sprite(start.x, start.y, "tiles", 25);
   player.setOrigin(0);
+  showFOV(start.x, start.y);
 
   /**
    * Create pointer marker
@@ -100,34 +103,74 @@ function update() {
    */
   marker.x = x;
   marker.y = y;
-
+  
   /**
    * Check if the next position if passable
    */
   tile = layer.getTileAtWorldXY(x, y);
   if (!tile.properties.unpassable) {
-
+    
     /** If passable turn the pointer yellow */
     marker.lineStyle(1, 0xffff00, 1);
     marker.strokeRect(0, 0, map.tileWidth, map.tileHeight);
-
+    
     /**
      * Check if the player clicked
      */
     if (this.input.manager.activePointer.justDown) {
-
+      
       /**
        * Move the player towards the pointer
        */
       player.x = x;
       player.y = y;
+
+      /**
+       * Update field of view
+       */
+      showFOV(x, y);
     }
   } else {
-
+    
     /**
      * If unpassable turn the pointer grey
      */
     marker.lineStyle(1, 0x888888, 1);
     marker.strokeRect(0, 0, map.tileWidth, map.tileHeight);
   }
+}
+
+function showFOV(x, y) {
+
+  /**
+   * Overlay fog of war on every tile that was already visible
+   */
+  layer.forEachTile(tile => (tile.alpha = tile.alpha ? 0.3 : 0));
+
+  /**
+   * Find the visible tiles
+   */
+  tileXY = layer.worldToTileXY(x, y);
+  fov.compute(tileXY.x, tileXY.y, 12, function (x, y) {
+
+    /**
+     * Show the visible tiles
+     */
+    var tile = layer.getTileAt(x, y);
+    if (tile) {
+      tile.alpha = 1;
+    }
+  });
+}
+
+function isTransparent(x, y) {
+  var playerXY, tile;
+
+  /**
+   * Return true if the position is the player's position or if it is not opaque
+   */
+  playerXY = layer.worldToTileXY(player.x, player.y);
+  tile = layer.getTileAt(x, y);
+  return x === playerXY.x && y === playerXY.y 
+    || tile && !tile.properties.opaque;
 }
