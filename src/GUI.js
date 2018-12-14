90e9d89cfd5ea61510c02310020c8cf86348939c
kosinaz <kosinaz@gmail.com>
2018-12-14 08:37:05 +0100

    Add equipment

--- src/GUI.js
+++ src/GUI.js
@@ -145,8 +145,10 @@ GUIScene = new Phaser.Class({
      * Create inventory
      */
     this.inventory = createInventory(this);
-    this.inventory.putTileAt(108, 0, 0);
-    this.inventory.putTileAt(102, 1, 0);
+    this.inventory.putTileAt(114, 0, 5);
+    this.inventory.putTileAt(138, 1, 5);
+    this.inventory.putTileAt(108, 2, 5);
+    this.inventory.putTileAt(102, 3, 5);
 
     /**
      * Grab a reference to the Game Scene
@@ -174,29 +176,218 @@ GUIScene = new Phaser.Class({
     
     x = this.input.activePointer.x;
     y = this.input.activePointer.y;
+
+    /**
+     * If the player clicked
+     */
     if (this.input.activePointer.justDown) {
-      tileXY = this.inventory.worldToTileXY(x, y);
-      tile = this.inventory.getTileAt(tileXY.x, tileXY.y);
-      if (tile) {
-        if (!this.hold) {
-          this.hold = this.add.image(x, y, "tiles", tile.index);
-          this.inventory.removeTileAt(tileXY.x, tileXY.y);
-        } 
-      } else if (this.hold) {
-        this.inventory.putTileAt(this.hold.frame.name, tileXY.x, tileXY.y);
-        this.hold.destroy();
-        this.hold = null;
+      tileXY = this.inventory.worldToTileXY(x, y);      
+      if (tileXY.y < 4) {
+        
+        /**
+         * If the player clicked on the equipment
+         */
+        if (tileXY.x === 7 && tileXY.y === 0) {
+
+          /**
+           * If the player clicked on the helmet
+           */
+          this.pickUpOrPutDownType("helmet", tileXY, x, y);
+        } else if (tileXY.x === 7 && tileXY.y === 1) {
+
+          /**
+           * If the player clicked on the necklace
+           */
+          this.pickUpOrPutDownType("necklace", tileXY, x, y);
+        } else if (tileXY.x === 6 && tileXY.y === 2) {
+
+          /**
+           * If the player clicked on the ring
+           */
+          this.pickUpOrPutDownType("ring", tileXY, x, y);
+        } else if (tileXY.x === 7 && tileXY.y === 2) {
+
+          /**
+           * If the player clicked on the cloak
+           */
+          this.pickUpOrPutDownType("cloak", tileXY, x, y);
+        } else if (tileXY.x === 8 && tileXY.y === 2) {
+
+          /**
+           * If the player clicked on the gloves
+           */
+          this.pickUpOrPutDownType("gloves", tileXY, x, y);
+        } else if (tileXY.x === 6 && tileXY.y === 3) {
+
+          /**
+           * If the player clicked on the weapon
+           */
+          this.pickUpOrPutDownType("weapon", tileXY, x, y);
+        } else if (tileXY.x === 7 && tileXY.y === 3) {
+
+          /**
+           * If the player clicked on the armor
+           */
+          this.pickUpOrPutDownType("armor", tileXY, x, y);
+          
+        } else if (tileXY.x === 8 && tileXY.y === 3) {
+
+          /**
+           * If the player clicked on the shield
+           */
+          this.pickUpOrPutDownType("shield", tileXY, x, y);
+        }         
+      } else if (tileXY.x > -1 && tileXY.x < 15
+        && (tileXY.y > 4 && tileXY.y < 15 
+        || tileXY.y > 15 && tileXY.y < 26)) {
+
+        /**
+         * If the player clicked on the inventory or on the ground
+         */
+        this.pickUpOrPutDown(tileXY, x, y);
       }
     }
 
+    /**
+     * Move the held item to the pointer
+     */
     if (this.hold) {
       this.hold.x = x;
       this.hold.y = y;
     }
     
-  }
+  },
 
+  isHolding: function (requiredItemType) {
+    var heldItemType = "";
+    if (!this.hold) {
+      return;
+    }
+    switch (this.hold.frame.name) {
+      case 100: heldItemType = "weapon"; break;
+      case 101: heldItemType = "armor"; break;
+      case 102: heldItemType = "shield"; break;
+      case 103: heldItemType = "weapon"; break;
+      case 104: heldItemType = "helmet"; break;
+      case 105: heldItemType = "shield"; break;
+      case 106: heldItemType = "weapon"; break;
+      case 107: heldItemType = "weapon"; break;
+      case 108: heldItemType = "weapon"; break;
+      case 109: heldItemType = ""; break;
+      case 110: heldItemType = ""; break;
+      case 111: heldItemType = "armor"; break;
+      case 112: heldItemType = "weapon"; break;
+      case 113: heldItemType = "cloak"; break;
+      case 114: heldItemType = "weapon"; break;
+      case 115: heldItemType = "weapon"; break;
+      case 116: heldItemType = "weapon"; break;
+      case 117: heldItemType = "weapon"; break;
+      case 118: heldItemType = "cloak"; break;
+      case 119: heldItemType = "necklace"; break;
+      case 120: heldItemType = "ring"; break;
+      case 121: heldItemType = "shield"; break;
+      case 122: heldItemType = "weapon"; break;
+      case 123: heldItemType = "gloves"; break;
+      case 124: heldItemType = "weapon"; break;
+      case 125: heldItemType = "gloves"; break;
+      case 126: heldItemType = "helmet"; break;
+      case 127: heldItemType = "armor"; break;
+      case 128: heldItemType = ""; break;
+      case 129: heldItemType = "necklace"; break;
+      case 130: heldItemType = "ring"; break;
+      case 131: heldItemType = "weapon"; break;
+      case 132: heldItemType = "cloak"; break;
+      case 133: heldItemType = "shield"; break;
+      case 134: heldItemType = "weapon"; break;
+      case 135: heldItemType = "weapon"; break;
+      case 136: heldItemType = "weapon"; break;
+      case 137: heldItemType = "weapon"; break;
+      case 138: heldItemType = "armor"; break;
+      case 139: heldItemType = "weapon"; break;
+      case 140: heldItemType = "armor"; break;
+      case 141: heldItemType = "weapon"; break;
+      case 142: heldItemType = "gloves"; break;
+      case 143: heldItemType = "helmet"; break;
+      case 144: heldItemType = "shield"; break;
+      case 145: heldItemType = "weapon"; break;
+      case 146: heldItemType = "weapon"; break;
+      case 147: heldItemType = "necklace"; break;
+      case 148: heldItemType = "weapon"; break;
+      case 149: heldItemType = "shield"; break;
+    }
+    return heldItemType === requiredItemType;
+  },
+
+  pickUpOrPutDownType: function (itemType, tileXY, x, y) {
+    if (this.hold) {
+      if (this.isHolding(itemType)) {
+
+        /**
+         * If the player holds someting put it down or switch it
+         */
+        this.pickUpOrPutDown(tileXY, x, y);
+      }
+    } else {
+
+      /** 
+       * If the player does not hold anything pick up if there is
+       * something 
+       */
+      this.pickUp(tileXY, x, y);
+    }
+  },
+
+  pickUpOrPutDown: function (tileXY, x, y) {
+    var tile = this.inventory.getTileAt(tileXY.x, tileXY.y);
+    if (this.hold) {
+
+      /**
+       * If the player holds something
+       */
+      var heldItem = this.hold.frame.name;
+      this.hold.destroy();
+      if (tile) {
+
+        /**
+         * If there is an item there pick up
+         */
+        this.hold = this.add.image(x, y, "tiles", tile.index);
+      }
+      else {
+
+        /**
+         * If there is no item there remove the already held one
+         */
+        this.hold = null;
+      }
+
+      /**
+       * Put down the already held item
+       */
+      this.inventory.putTileAt(heldItem, tileXY.x, tileXY.y);
+    }
+    else {
+
+      /** 
+       * If the player does not hold anything pick up if there is something 
+       */
+      this.pickUp(tileXY, x, y);
+    }
+  },
+  
+  pickUp: function (tileXY, x, y) {
+    var tile = this.inventory.getTileAt(tileXY.x, tileXY.y);
+    if (tile) {
+
+      /**
+       * If the player does not hold anything pick up the item
+       */
+      this.hold = this.add.image(x, y, "tiles", tile.index);
+      this.inventory.removeTileAt(tileXY.x, tileXY.y);
+    }
+  }
 });
+
 
 /**
  * Create the inventory of the player
@@ -207,12 +398,12 @@ function createInventory(scene) {
     tileWidth: 24,
     tileHeight: 21,
     width: 15,
-    height: 10
+    height: 26
   });
   return map.createBlankDynamicLayer(
     'inventory', 
     map.addTilesetImage('tiles'), 
     4, 
-    110
+    5
   );
 }
