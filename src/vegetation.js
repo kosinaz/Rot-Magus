8e632573497b452e01b986c22214e335dd86f8b9
kosinaz <kosinaz@gmail.com>
2019-01-09 08:22:25 +0100

    Fine tune vegetation

--- src/vegetation.js
+++ src/vegetation.js
@@ -6,7 +6,7 @@ const Vegetation = {
         let XY = position.split(',');
         let density = noise.get(XY[0] / 64, XY[1] / 64) * 2 + 2;
         let tile;
-        if (Math.random() * density > 3) {
+        if (Math.random() * density > 2) {
           tile = layer.putTileAt(Math.random() > 0.3 ? 17 : 16, XY[0], XY[1]);
           if (!mapdebug) {
             tile.properties = {
