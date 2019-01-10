66d61533f756fa1004d589c56ee7aae9404e145a
kosinaz <kosinaz@gmail.com>
2019-01-10 08:42:09 +0100

    Improve vegetation

--- src/vegetation.js
+++ src/vegetation.js
@@ -5,17 +5,26 @@ const Vegetation = {
       area.forEach(function (position) {
         let XY = position.split(',');
         let density = noise.get(XY[0] / 64, XY[1] / 64) * 2 + 2;
-        let tile;
-        if (Math.random() * density > 2) {
-          tile = layer.putTileAt(Math.random() > 0.3 ? 17 : 16, XY[0], XY[1]);
-          if (!mapdebug) {
-            tile.properties = {
-              unpassable: true,
-              opaque: true
+        let tile = layer.getTileAt(XY[0] - 1, XY[1]);
+        if (tile && !tile.index || Math.random() > 0.75) {
+          tile = layer.getTileAt(XY[0], XY[1] - 1);
+          if (tile && !tile.index || Math.random() > 0.75) {
+            if (Math.random() * density > 2 || Math.random() > 0.95) {
+              tile = layer.putTileAt(Math.random() > 0.3 ? 17 : 16, XY[0], XY[1]);
+              if (!mapdebug) {
+                tile.properties = {
+                  unpassable: true,
+                  opaque: true
+                }
+              }
+            } else if (Math.random() * density < 0.03) {
+              layer.putTileAt(Math.random() > 0.5 ? 1 : 2, XY[0], XY[1]);
+            } else {
+              layer.putTileAt(0, XY[0], XY[1]);
             }
+          } else {
+            layer.putTileAt(0, XY[0], XY[1]);
           }
-        } else if (Math.random() * density < 0.03) {
-          layer.putTileAt(Math.random() > 0.5 ? 1 : 2, XY[0], XY[1]);
         } else {
           layer.putTileAt(0, XY[0], XY[1]);
         }
