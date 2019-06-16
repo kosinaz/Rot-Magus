f4ccadd842d57c9905d7a7683ce94bc74531dc3f
kosinaz <kosinaz@gmail.com>
2019-06-16 21:02:52 +0200

    v0.4.0 A new phase

--- source/item.js
+++
@@ -1,9 +0,0 @@
-/*global RM*/
-RM.Item = function (type) {
-  'use strict';
-  this.type = type;
-  if (type.count) {
-    this.count = type.count;
-  }
-  this.tile = type.tile;
-};
--- source/titlescreen.js
+++
@@ -1,9 +0,0 @@
-/*global RM*/
-RM.TitleScreen = function (background) {
-  'use strict';
-  this.background = RM.title;
-  this.uiObjects = [
-    new RM.Label(250, 40, 140, 21, RM.VERSION, '#616161'),
-    new RM.Button(250, 440, 140, 28, 'Start Game', RM.start)
-  ];
-};
